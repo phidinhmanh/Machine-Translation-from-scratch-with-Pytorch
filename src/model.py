62fec8632d10e9cd173bba5705ab6598cc87caf5
kymy <ongnoitv3@hmail.com>
2025-12-07 08:19:29 +0700

    fix error_5

--- src/model.py
+++ src/model.py
@@ -3,23 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
 class PositionalEncoding(nn.Module):
     def __init__(self, embed_dim, max_len=5000):
         super(PositionalEncoding, self).__init__()
         pe = torch.zeros(max_len, embed_dim)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, embed_dim, 2).float() * -(torch.log(torch.tensor(10000.0)) / embed_dim))
+        div_term = torch.exp(
+            torch.arange(0, embed_dim, 2).float()
+            * -(torch.log(torch.tensor(10000.0)) / embed_dim)
+        )
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
+        self.register_buffer("pe", pe)
 
     def forward(self, x):
-        x = x + self.pe[:, :x.size(1)] # type: ignore
+        x = x + self.pe[:, : x.size(1)]  # type: ignore
         return x
-
-
 
 
 class MultiHeadAttention(torch.nn.Module):
@@ -32,8 +32,6 @@ class MultiHeadAttention(torch.nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.q_proj = nn.Linear(embed_dim, embed_dim)
         self.kv_proj = nn.Linear(embed_dim, 2 * embed_dim)
-
-
 
     def forward(self, x, mask=None, context=None):
         batch_size, seq_len, embed_dim = x.size()
@@ -48,25 +46,33 @@ class MultiHeadAttention(torch.nn.Module):
         k = k.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
         v = v.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
 
-        attn_scores = torch.matmul(q, k.transpose(-2, -1)) / (self.head_dim ** 0.5)
+        attn_scores = torch.matmul(q, k.transpose(-2, -1)) / (self.head_dim**0.5)
         if mask is not None:
-            attn_scores = attn_scores.masked_fill(mask == 0, -float('inf'))
+            attn_scores = attn_scores.masked_fill(mask == 0, -float("inf"))
         attn_probs = F.softmax(attn_scores, dim=-1)
         attn_probs = self.dropout(attn_probs)
         attn_output = torch.matmul(attn_probs, v)
         # recombie head
-        attn_output = attn_output.transpose(1, 2).contiguous().view(batch_size, seq_len, embed_dim)
+        attn_output = (
+            attn_output.transpose(1, 2)
+            .contiguous()
+            .view(batch_size, seq_len, embed_dim)
+        )
         attn_output = self.out_proj(attn_output)
         return attn_output
 
 
-
-
-
 class TransformerBlock(nn.Module):
-    def __init__(self, embed_dim, num_heads, dropout=0.1, feedforward_expansion=3,is_decoder=False):
+    def __init__(
+        self,
+        embed_dim,
+        num_heads,
+        dropout=0.1,
+        feedforward_expansion=3,
+        is_decoder=False,
+    ):
         super(TransformerBlock, self).__init__()
-        self.is_decoder = is_decoder
+        self.is_decoder = is_decoder  # ty:ignore[unresolved-attribute]
         self.self_attn = MultiHeadAttention(embed_dim, num_heads, dropout)
         self.norm1 = nn.RMSNorm(embed_dim)
         self.dropout1 = nn.Dropout(dropout)
@@ -80,11 +86,12 @@ class TransformerBlock(nn.Module):
             nn.Linear(embed_dim, feedforward_expansion * embed_dim),
             nn.GELU(),
             nn.Dropout(dropout),
-            nn.Linear(feedforward_expansion * embed_dim, embed_dim)
+            nn.Linear(feedforward_expansion * embed_dim, embed_dim),
         )
         self.norm3 = nn.RMSNorm(embed_dim)
         self.dropout3 = nn.Dropout(dropout)
-    def forward(self, x, mask, context = None,src_mask=None):
+
+    def forward(self, x, mask, context=None, src_mask=None):
         atten_out = self.self_attn(x, mask=mask)
         x = x + self.dropout1(atten_out)
         x = self.norm1(x)
@@ -100,38 +107,48 @@ class TransformerBlock(nn.Module):
         return x
 
 
-
-
-
 class Transformer(nn.Module):
-    def __init__(self,vocab_size,embed_dim, heads, blocks,device ,dropout=0.1, ff_expansion = 3) -> None:
+    def __init__(
+        self, vocab_size, embed_dim, heads, blocks, device, dropout=0.1, ff_expansion=3
+    ) -> None:
         super(Transformer, self).__init__()
         self.embed_dim = embed_dim
         self.device = device
         self.pos_encoder = PositionalEncoding(embed_dim)
         self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.transformer_encoder = nn.ModuleList([
-            TransformerBlock(embed_dim, heads, dropout, ff_expansion) for _ in range(blocks)
-        ])
-        self.transformer_decoder = nn.ModuleList([
-            TransformerBlock(embed_dim, heads, dropout, ff_expansion, is_decoder=True) for _ in range(blocks)
-        ])
+        self.transformer_encoder = nn.ModuleList(
+            [
+                TransformerBlock(embed_dim, heads, dropout, ff_expansion)
+                for _ in range(blocks)
+            ]
+        )
+        self.transformer_decoder = nn.ModuleList(
+            [
+                TransformerBlock(
+                    embed_dim, heads, dropout, ff_expansion, is_decoder=True
+                )
+                for _ in range(blocks)
+            ]
+        )
         self.dropout = nn.Dropout(dropout)
         self.linear = nn.Linear(embed_dim, vocab_size)
         self.final_norm = nn.RMSNorm(embed_dim)
         self.linear.weight = self.embedding.weight
+        self.apply(self._init_weights)
 
-    
     def make_src_mask(self, src):
         src_mask = (src != 0).unsqueeze(1).unsqueeze(2).to(self.device)
         return src_mask
-    
+
     def make_trg_mask(self, trg):
         N, trg_len = trg.shape
         trg_mask = (trg != 0).unsqueeze(1).unsqueeze(2).to(self.device)
-        tril = torch.tril(torch.ones((trg_len, trg_len), dtype=torch.bool, device=self.device)).expand(N, 1, trg_len, trg_len)
+        tril = torch.tril(
+            torch.ones((trg_len, trg_len), dtype=torch.bool, device=self.device)
+        ).expand(N, 1, trg_len, trg_len)
         trg_mask = trg_mask & tril
         return trg_mask
+
     def forward(self, src, trg):
         src_mask = self.make_src_mask(src)
         trg_mask = self.make_trg_mask(trg)
@@ -152,25 +169,9 @@ class Transformer(nn.Module):
             nn.init.xavier_uniform_(module.weight)
             if module.bias is not None:
                 nn.init.constant_(module.bias, 0)
-        
+
         elif isinstance(module, nn.Embedding):
             nn.init.normal_(module.weight, mean=0, std=0.1)
-        
+
         elif isinstance(module, nn.RMSNorm):
             nn.init.constant_(module.weight, 1.0)
-    
-
-
-
-
-    
-
-        
-        
-
-
-
-
-
-
-        
--- src/train.py
+++ src/train.py
@@ -5,124 +5,170 @@ import argparse
 import os
 from tqdm import tqdm
 from datasets import load_dataset
-
-# Import các module em đã viết
-# Giả sử file Transformer nằm ở src/model.py
-# Giả sử hàm get_dataloader nằm ở src/data_loader.py
-from model import Transformer  
+from model import Transformer
 from preprocess import getdata_loader
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filename="train.log",
+    filemode="w",
+)
+logger = logging.getLogger(__name__)
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Train Transformer Vi-En")
-    
+
     # Data params
     parser.add_argument("--max_len", type=int, default=128, help="Max sequence length")
-    parser.add_argument("--vocab_size", type=int, default=8000, help="Vocab size (khớp với SPM)")
-    
+    parser.add_argument(
+        "--vocab_size", type=int, default=8000, help="Vocab size (khớp với SPM)"
+    )
+
     # Model params
     parser.add_argument("--embed_dim", type=int, default=128)
     parser.add_argument("--heads", type=int, default=4)
     parser.add_argument("--layers", type=int, default=3)
     parser.add_argument("--ff_expansion", type=int, default=4)
     parser.add_argument("--dropout", type=float, default=0.1)
-    
+
     # Training params
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--epochs", type=int, default=2)
     parser.add_argument("--lr", type=float, default=3e-4)
-    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu")
+    parser.add_argument(
+        "--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu"
+    )
     parser.add_argument("--save_dir", type=str, default="checkpoints")
     parser.add_argument("--pad_idx", type=int, default=0)
-    
+
     return parser.parse_args()
+
 
 def train_one_epoch(model, loader, optimizer, criterion, scaler, device, epoch):
     model.train()
     total_loss = 0
-    
+
     # Thanh progress bar
     loop = tqdm(loader, desc=f"Training Epoch {epoch}")
-    
-    for batch in loop:
+
+    optimizer.zero_grad()
+
+    for step, batch in enumerate(loop):
         # 1. Lấy dữ liệu & đẩy lên GPU
         # src: [Batch, Src_Len]
         # tgt: [Batch, Tgt_Len] (Gồm cả BOS và EOS)
-        src = batch['src_ids'].to(device)
-        tgt = batch['tgt_ids'].to(device)
-        
+        src = batch["src_ids"].to(device)
+        tgt = batch["tgt_ids"].to(device)
+
         # 2. Chuẩn bị Input và Label (Teacher Forcing)
         # Input vào Decoder: Bỏ token CUỐI (EOS) -> [BOS, A, B, C]
-        decoder_input = tgt[:, :-1] 
-        
+        decoder_input = tgt[:, :-1]
+
         # Label để tính Loss: Bỏ token ĐẦU (BOS) -> [A, B, C, EOS]
         labels = tgt[:, 1:]
 
         # 3. Forward Pass (Dùng Mixed Precision cho nhanh)
-        # optimizer.zero_grad() ở đây hoặc cuối loop đều được
-        optimizer.zero_grad()
-        
-        with torch.amp.autocast_mode.autocast(enabled=(device == "cuda"), device_type=device):
+        if step % 4 == 0:
+            optimizer.zero_grad()
+
+        with torch.amp.autocast_mode.autocast(
+            enabled=(device == "cuda"), device_type=device
+        ):
             # Output: [Batch, Seq_Len, Vocab_Size]
             output = model(src, decoder_input)
-            
+
+            # print(f"Is NaN: {torch.isnan(output).any()}")
+            # print(f"min and max: {output.min().item()}, {output.max().item()}")
+
             # Reshape để tính Loss
             # Output: [Batch * Seq_Len, Vocab]
             # Labels: [Batch * Seq_Len]
-            loss = criterion(
-                output.reshape(-1, output.shape[-1]), 
-                labels.reshape(-1)
-            )
+            loss = criterion(output.reshape(-1, output.shape[-1]), labels.reshape(-1))
 
         # 4. Backward Pass (Backward + Optimize)
         scaler.scale(loss).backward()
-        
+
         scaler.unscale_(optimizer)
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        
+
         scaler.step(optimizer)
         scaler.update()
-        
+
         # 5. Logging
         total_loss += loss.item()
         loop.set_postfix(loss=loss.item())
-        
+
+        # check gradient
+        for name, param in model.named_parameters():
+            if param.grad is None:
+                print(f"Gradient is None for {name}")
+                break
+            if param.grad.isnan().any():
+                print(f"Gradient is NaN for {name}")
+                break
+            if param.grad.sum() == 0:
+                print(f"Gradient is zero for {name}")
+                break
+        # check memory available
+        print(f"Memory available: {torch.cuda.memory_allocated() / 1e9}")
+
+        # check loss is nan
+        if torch.isnan(loss):
+            print("Loss is NaN")
+            break
+        # if step == 200:
+        #     return total_loss / 200
+
     return total_loss / len(loader)
+
 
 def evaluate(model, loader, criterion, device):
     model.eval()
     total_loss = 0
-    
+
     with torch.no_grad():
         for batch in loader:
-            src = batch['src_ids'].to(device)
-            tgt = batch['tgt_ids'].to(device)
-            
+            src = batch["src_ids"].to(device)
+            tgt = batch["tgt_ids"].to(device)
+
             decoder_input = tgt[:, :-1]
             labels = tgt[:, 1:]
-            
+
             output = model(src, decoder_input)
-            
-            loss = criterion(
-                output.reshape(-1, output.shape[-1]), 
-                labels.reshape(-1)
-            )
+
+            loss = criterion(output.reshape(-1, output.shape[-1]), labels.reshape(-1))
             total_loss += loss.item()
-            
+
     return total_loss / len(loader)
+
 
 def main():
     args = get_args()
     print(f"🚀 Config: {vars(args)}")
     os.makedirs(args.save_dir, exist_ok=True)
-    
+
     # 1. Load Data
     # Lưu ý: Em phải chắc chắn máy có internet để load opus100, hoặc trỏ vào local
     print("⏳ Loading Datasets...")
     datasets = load_dataset("opus100", "en-vi")
-    train_loader = getdata_loader(datasets, batch_size=args.batch_size, max_len=args.max_len, shuffle=True, type='train')
-    val_loader = getdata_loader(datasets, batch_size=args.batch_size, max_len=args.max_len, shuffle=False, type='validation')
-    
-    
+    train_loader = getdata_loader(
+        datasets,
+        batch_size=args.batch_size,
+        max_len=args.max_len,
+        shuffle=True,
+        type="train",
+    )
+    val_loader = getdata_loader(
+        datasets,
+        batch_size=args.batch_size,
+        max_len=args.max_len,
+        shuffle=False,
+        type="validation",
+    )
+
     # 2. Init Model
     print("🏗️ Initializing Model...")
     model = Transformer(
@@ -131,41 +177,53 @@ def main():
         heads=args.heads,
         blocks=args.layers,
         dropout=args.dropout,
-        ff_expansion=args.ff_expansion,# Truyền 0 vào đây
-        device=args.device    # Cần thiết cho việc tạo mask
+        ff_expansion=args.ff_expansion,  # Truyền 0 vào đây
+        device=args.device,  # Cần thiết cho việc tạo mask
     ).to(args.device)
-    
+
     # Đếm tham số chơi cho vui
-    print(f"Test Parameter Count: {sum(p.numel() for p in model.parameters() if p.requires_grad):,}")
+    print(
+        f"Test Parameter Count: {sum(p.numel() for p in model.parameters() if p.requires_grad):,}"
+    )
 
     # 3. Setup Training
     # AdamW là chân ái cho Transformer (Weight Decay giúp chống Overfit)
     optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=1e-4)
-    
+
     # Loss function: Ignore index 0 (PAD) để không tính điểm cho padding
     criterion = nn.CrossEntropyLoss(ignore_index=args.pad_idx, label_smoothing=0.1)
-    
+
     # Scaler cho Mixed Precision
     scaler = torch.amp.grad_scaler.GradScaler(enabled=(args.device == "cuda"))
-    
+
     # Scheduler (Giảm LR khi Loss không giảm nữa)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="min", factor=0.5, patience=2
+    )
 
     # 4. Training Loop
-    best_val_loss = float('inf')
-    
+    best_val_loss = float("inf")
+
     print("🔥 Start Training...")
     for epoch in range(1, args.epochs + 1):
         # Train
-        train_loss = train_one_epoch(model, train_loader, optimizer, criterion, scaler, args.device, epoch)
+        train_loss = train_one_epoch(
+            model, train_loader, optimizer, criterion, scaler, args.device, epoch
+        )
         # Validate
         val_loss = evaluate(model, val_loader, criterion, args.device)
         # Update LR
         scheduler.step(val_loss)
-        current_lr = optimizer.param_groups[0]['lr']
-        
-        print(f"Epoch {epoch} | Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | LR: {current_lr:.6f}")
-        
+        current_lr = optimizer.param_groups[0]["lr"]
+
+        print(
+            f"Epoch {epoch} | Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | LR: {current_lr:.6f}"
+        )
+
+        logger.info(
+            f"Epoch {epoch} | Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | LR: {current_lr:.6f}"
+        )
+
         # Save Best Model
         if val_loss < best_val_loss:
             best_val_loss = val_loss
@@ -174,6 +232,7 @@ def main():
             print(f"✅ Saved Best Model to {save_path}")
 
     print("🎉 Training Completed!")
+
 
 if __name__ == "__main__":
     main()
